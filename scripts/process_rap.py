411b9d93e4fa7470c2ce799ac3d937a6f0a09090
alenzi35 <alenzi3500@gmail.com>
2026-01-27 07:29:34 +0000

    Update process_rap.py

--- scripts/process_rap.py
+++ scripts/process_rap.py
@@ -74,7 +74,7 @@ linear_comb = (
     + cape * coeffs["CAPE"]
     + cin  * coeffs["CIN"]
     + hlcy * coeffs["HLCY"]
-)prob = 1 / (1 + np.exp(-linear_comb))  # logistic function
+prob = 1 / (1 + np.exp(-linear_comb))  # logistic function
 
 # -------------------------------
 # 8️⃣ Build JSON for map
