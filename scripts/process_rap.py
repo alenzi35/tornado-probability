696bc0a4687d80c434006000f1c0ae2762f30047
alenzi35 <alenzi3500@gmail.com>
2026-02-28 20:38:24 +0000

    Update process_rap.py

--- scripts/process_rap.py
+++ scripts/process_rap.py
@@ -30,7 +30,7 @@ COEFFS = {
 
 # ===== MANUAL DATE CONTROL =====
 # Set to None to use latest available cycle
-MANUAL_DATE = "20250315"   # "YYYYMMDD" or None
+MANUAL_DATE = "20250620"   # "YYYYMMDD" or None
 MANUAL_HOUR = "17"         # "HH" or None
 
 # US Census lower 48 states 5m shapefile
