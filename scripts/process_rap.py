0a630cdc71093be37de87f52663e2305f179e869
alenzi35 <alenzi3500@gmail.com>
2026-03-04 01:07:30 +0000

    Update process_rap.py

--- scripts/process_rap.py
+++ scripts/process_rap.py
@@ -31,8 +31,8 @@ COEFFS = {
 
 # === OPTIONAL MANUAL DATE TESTING ===
 # Leave as None for automatic RAP cycle
-CUSTOM_DATE = 20250621 # example: "20250427"
-CUSTOM_HOUR = 0o4  # example: "21"
+CUSTOM_DATE = 20250620 # example: "20250427"
+CUSTOM_HOUR = 23  # example: "21"
 
 FCST = "00"
 
