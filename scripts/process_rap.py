0325e5f9744a0ff481d47c62375614e84be63995
alenzi35 <alenzi3500@gmail.com>
2026-03-04 00:15:00 +0000

    Update process_rap.py

--- scripts/process_rap.py
+++ scripts/process_rap.py
@@ -31,10 +31,10 @@ COEFFS = {
 
 # === OPTIONAL MANUAL DATE TESTING ===
 # Leave as None for automatic RAP cycle
-CUSTOM_DATE = None  # example: "20250427"
-CUSTOM_HOUR = None  # example: "21"
+CUSTOM_DATE = 20250621  # example: "20250427"
+CUSTOM_HOUR = 04  # example: "21"
 
-FCST = "01"
+FCST = "00"
 
 # US Census lower 48 states shapefile
 CONUS_SHAPE_URL = "https://www2.census.gov/geo/tiger/GENZ2024/shp/cb_2024_us_state_5m.zip"
