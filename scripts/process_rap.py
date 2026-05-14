8af07a3260bd0fbe86e3510300aada8577d9f7f9
alenzi35 <alenzi3500@gmail.com>
2026-05-14 23:21:45 +0100

    Update process_rap.py

--- scripts/process_rap.py
+++ scripts/process_rap.py
@@ -19,7 +19,7 @@ DATA_DIR = "data"
 GRIB_PATH = "data/rap.grib2"
 OUTPUT_JSON = "map/data/tornado_prob_lcc.json"
 
-INTERCEPT = -24.3
+INTERCEPT = -24.35
 
 COEFFS = {
     "cape": 0.0007945647876223339,
