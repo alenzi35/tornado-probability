fa0aa0860982bc0a28dfe5caf0930678ac6d9eaa
alenzi35 <alenzi3500@gmail.com>
2026-06-02 23:47:57 +0100

    Update process_rap.py

--- scripts/process_rap.py
+++ scripts/process_rap.py
@@ -198,9 +198,6 @@ prob = 1 / (1 + np.exp(-linear))
 print("Current mean probability (decimal):", np.mean(prob))
 print("Current mean probability (%):", np.mean(prob) * 100)
 
-max_prob = np.max(prob)
-print("Maximum probability (%):", max_prob * 100)
-
 # ================= DOWNLOAD CONUS SHAPE =================
 
 def download_shapefile(url, folder):
